1d6776152b71fd5d0f1711fc437452ae52417288
Bernardo Viggiano <126487673+beviggiano@users.noreply.github.com>
2025-11-10 17:54:29 -0300

    Add edit and delete handlers to FeatureFlagList

--- packages/web-client/src/components/FeatureFlagList/FeatureFlagList.tsx
+++ packages/web-client/src/components/FeatureFlagList/FeatureFlagList.tsx
@@ -1,22 +1,29 @@
 import useFeatureFlags from "../../hooks/useFeatureFlags";
 import FeatureFlagItem from "./FeatureFlagItem/FeatureFlagItem";
 
+interface FeatureFlagListProps {
+  onEdit: (flag: { id: number; name: string; description: string; status: boolean }) => void;
+  onDelete: (flag: { id: number; name: string; description: string; status: boolean }) => void;
+}
 
-export default function FeatureFlagList() {
-    const { data, deleteFlag, toggleFlag } = useFeatureFlags();
+export default function FeatureFlagList({ onEdit, onDelete }: FeatureFlagListProps) {
+  const { data, deleteFlag, toggleFlag } = useFeatureFlags();
 
-    return (
-        <ul className=" flex flex-col gap-5 mt-5">
-            {data.map(flag =>
-                <FeatureFlagItem
-                    key={flag.id}
-                    id={flag.id}
-                    name={flag.name}
-                    description={flag.description}
-                    status={flag.status}
-                    deleteFlag={deleteFlag}
-                    toogleFlag={toggleFlag} />
-            )}
-        </ul>
-    )
+  return (
+    <ul className="flex flex-col gap-5 mt-5">
+      {data.map((flag) => (
+        <FeatureFlagItem
+          key={flag.id}
+          id={flag.id}
+          name={flag.name}
+          description={flag.description}
+          status={flag.status}
+          deleteFlag={deleteFlag}
+          toogleFlag={toggleFlag}
+          onEdit={() => onEdit(flag)}
+          onDelete={() => onDelete(flag)}
+        />
+      ))}
+    </ul>
+  );
 }
